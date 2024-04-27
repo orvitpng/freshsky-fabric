80c4f9409755a43e5954c4f1d070ad1b9505dc97
Carter Davis <cdavis4short@gmail.com>
2024-04-27 04:47:14 -0500

    add `chat_combiner` base

--- docs/modules/no_swim.md
+++
@@ -1,4 +0,0 @@
-# no_swim
-
-Disables sprinting while swimming, which was introduced in 1.13 and causes
-problems on Hypixel.
---
+++ src/main/java/ninja/carter/mods/freshsky/modules/chat_combiner/ChatCombinerModule.java
@@ -0,0 +1,5 @@
+package ninja.carter.mods.freshsky.modules.chat_combiner;
+
+public class ChatCombinerModule {
+	public static boolean ENABLED = true;
+}
